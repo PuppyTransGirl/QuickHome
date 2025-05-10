32a30b54fd1b5f4a8a0778e2f1db84923ae10126
Toutouchien <toutouchienshiba@gmail.com>
2025-05-10 15:13:25 +0200

    Implement home loading in HomeManager

--- src/main/java/toutouchien/quickhome/managers/HomeManager.java
+++ src/main/java/toutouchien/quickhome/managers/HomeManager.java
@@ -1,16 +1,24 @@
 package toutouchien.quickhome.managers;
 
 import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.PermissionAttachmentInfo;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import toutouchien.quickhome.QuickHome;
 import toutouchien.quickhome.lang.Lang;
 import toutouchien.quickhome.models.Home;
 
+import java.io.File;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class HomeManager {
+    private final QuickHome plugin;
+    private final File homesFolder;
+
     private final Map<UUID, List<Home>> homes;
 
     public HomeManager() {
@@ -70,12 +78,78 @@ public class HomeManager {
 
     @NotNull
     public List<Home> homes(@NotNull UUID uuid) {
-        return this.homes.computeIfAbsent(uuid, k -> Collections.emptyList());
+        return this.homes.computeIfAbsent(uuid, this::loadHomes);
     }
 
     public void teleportToHome(@NotNull Player player, @NotNull Home home) {
         player.teleportAsync(home.location()).thenAccept(v -> {
             Lang.sendMessage(player, "home_teleport_success", home.name());
         });
+    }
+
+    public List<Home> loadHomes(@NotNull UUID uuid) {
+        List<Home> loadedHomes = new ArrayList<>();
+
+        File file = new File(homesFolder, uuid + ".yml");
+        if (!file.exists())
+            return Collections.emptyList();
+
+        FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+        for (String key : config.getKeys(false)) {
+            String worldName = config.getString(key + ".world");
+            double x = config.getDouble(key + ".x");
+            double y = config.getDouble(key + ".y");
+            double z = config.getDouble(key + ".z");
+            float yaw = (float) config.getDouble(key + ".yaw");
+            float pitch = (float) config.getDouble(key + ".pitch");
+
+            Location location = new Location(plugin.getServer().getWorld(worldName), x, y, z, yaw, pitch);
+            loadedHomes.add(new Home(key, location));
+        }
+
+        return Collections.unmodifiableList(loadedHomes);
+    }
+
+    public void saveHomes() {
+        Set<Map.Entry<UUID, List<Home>>> entries = this.homes.entrySet();
+        for (Map.Entry<UUID, List<Home>> entry : entries) {
+            UUID uuid = entry.getKey();
+            List<Home> cachedHomes = entry.getValue();
+
+            File file = new File(homesFolder, uuid + ".yml");
+            FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+
+            Set<String> homesKeys = config.getKeys(false);
+            Set<String> homesNames = cachedHomes.stream()
+                    .map(home -> home.name().toLowerCase(Locale.ROOT))
+                    .collect(Collectors.toSet());
+
+            // Delete all homes that are not in the cache but that are in the home file
+            for (String key : homesKeys) {
+                if (homesNames.contains(key))
+                    continue;
+
+                config.set("homes." + key, null);
+            }
+
+            // Save all homes in the cache
+            for (Home home : cachedHomes) {
+                String key = home.name().toLowerCase(Locale.ROOT);
+                Location location = home.location();
+
+                config.set(key + ".world", location.getWorld().getName());
+                config.set(key + ".x", location.getX());
+                config.set(key + ".y", location.getY());
+                config.set(key + ".z", location.getZ());
+                config.set(key + ".yaw", location.getYaw());
+                config.set(key + ".pitch", location.getPitch());
+            }
+
+            try {
+                config.save(file);
+            } catch (Exception e) {
+                plugin.getSLF4JLogger().warn("Failed to save homes for " + uuid, e);
+            }
+        }
     }
 }
